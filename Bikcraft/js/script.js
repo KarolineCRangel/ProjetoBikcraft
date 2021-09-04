a96e8197ee3e6c5f7f098a0588f7f0ac82e0a826
Karoline Rangel <55639371+KarolineCRangel@users.noreply.github.com>
2021-09-04 20:20:39 -0300

    Add files via upload

---
+++ Bikcraft/js/script.js
@@ -0,0 +1,17 @@
+
+if(window.SimpleSlide) {
+    new SimpleSlide ({
+        slide: "quote",
+        time: 5000 //5 segundos
+    })
+
+    new SimpleSlide ({
+        slide: "portfolio",
+        nav: true,
+        time: 5000 //5 segundos
+    })
+}
+
+if (window.SimpleAnime) {
+    new SimpleAnime();
+}
